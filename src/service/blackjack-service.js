b31d53b60cada7d8c779837d07f845dcb5268524
Finnishandy <finnishnandy@gmail.com>
2015-07-02 19:30:39 +0100

    new files

--- src/service/blackjack-service.js
+++ src/service/blackjack-service.js
@@ -1,22 +1,23 @@
 /**
  * Test service with:
- * curl -v -H "Content-Type: application/json" -XPOST --data "{}" localhost:3000/api/deal
+ * curl -v -H "Content-Type: application/json" -XGET --data "{}" localhost:3000/api/deal
  * @type {*|exports|module.exports}
  */
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var Deal = require('../db/models/deal');
 
 var app = express();
 app.use(bodyParser.json());
 
 app.get('/api/deal', function(req, res, next) {
    var deal = new Deal({
-       username: req.body.username,
+       players: req.body.players,
        body: req.body.body
     });
     console.log(deal);
-    res.json(201, deal);
+    res.status(201).json(deal);
     /*
     deal.save(function(err,post)  {
         if (err) {return next(err);}
